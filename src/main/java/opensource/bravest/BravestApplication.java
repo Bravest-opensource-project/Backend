fc5b88d7f5ac9878aa0e3995efa39f95307b1dea
2heunxun <seapeon@naver.com>
2025-11-07 11:50:04 +0900

    Initialize Spring Boot project

---
+++ src/main/java/opensource/bravest/BravestApplication.java
@@ -0,0 +1,13 @@
+package opensource.bravest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BravestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/opensource/bravest/BravestApplicationTests.java
@@ -0,0 +1,13 @@
+package opensource.bravest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BravestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
