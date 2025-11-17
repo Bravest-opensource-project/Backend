0e5d8c22263f78264348f23b6309602189aa8db9
2heunxun <seapeon@naver.com>
2025-11-18 00:58:56 +0900

    [feature] api pay loader setting

---
+++ src/main/java/opensource/bravest/global/apiPayload/code/BaseCode.java
@@ -0,0 +1,6 @@
+package opensource.bravest.global.apiPayload.code;
+
+public interface BaseCode {
+  ReasonDto getReason();
+  ReasonDto getReasonHttpStatus();
+}
---
+++ src/main/java/opensource/bravest/global/exception/CustomException.java
@@ -0,0 +1,19 @@
+package opensource.bravest.global.exception;
+
+
+import lombok.Getter;
+import opensource.bravest.global.apiPayload.code.BaseErrorCode;
+
+/**
+ * 서비스/도메인 레이어에서 표준화된 에러코드를 던지기 위한 예외
+ */
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final BaseErrorCode errorCode;
+
+    public CustomException(BaseErrorCode errorCode) {
+        super(errorCode.getReason().getMessage());
+        this.errorCode = errorCode;
+    }
+}
