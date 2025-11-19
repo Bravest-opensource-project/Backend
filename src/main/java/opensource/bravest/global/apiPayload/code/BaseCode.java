904ce81eefaa26ba279ea583d75177d3fe61a81d
YeongHu Jang <128132449+JangYeongHu@users.noreply.github.com>
2025-11-19 20:36:18 +0900

    Revert "Feature/chatlist api"

--- src/main/java/opensource/bravest/global/apiPayload/code/BaseCode.java
+++
@@ -1,6 +0,0 @@
-package opensource.bravest.global.apiPayload.code;
-
-public interface BaseCode {
-  ReasonDto getReason();
-  ReasonDto getReasonHttpStatus();
-}
--- src/main/java/opensource/bravest/global/apiPayload/code/BaseErrorCode.java
+++
@@ -1,6 +0,0 @@
-package opensource.bravest.global.apiPayload.code;
-
-public interface BaseErrorCode {
-  ErrorReasonDto getReason();
-  ErrorReasonDto getReasonHttpStatus();
-}
